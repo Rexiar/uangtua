35cd36ad90ee37fdd3c199899e8570b6c1ff0b83
Lia <dhiyaa.amalia.putri@mail.ugm.ac.id>
2023-11-01 12:03:24 +0700

    Services Income

--- MainProgram/Service/IncomeService.cs
+++ MainProgram/Service/IncomeService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MainProgram.Config;
+using MainProgram.Model;
+using Npgsql;
 
 namespace MainProgram.Service
 {
-    internal class IncomeService
+    public class IncomeService
     {
+        public static bool AddIncome(Income income)
+        {
+            DBConnection dbConnection = new DBConnection();
+            string query = "INSERT INTO Income (IncomeId, Title, CatagoryId, Amount, TypeIncome, Description, CreatedAt, UserId) VALUES (@incomeid, @title, @catagoryid, @amount, @type, @description, @createAt, @userid)";
+            using (NpgsqlConnection connection = dbConnection.GetConnection())
+            {
+                connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@incomeid", income.IncomeId);
+                    command.Parameters.AddWithValue("@title", income.Title);
+                    command.Parameters.AddWithValue("@catagoryid", income.CatagoryId);
+                    command.Parameters.AddWithValue("@amount", income.Amount);
+                    command.Parameters.AddWithValue("@type", income.TypeIncome);
+                    command.Parameters.AddWithValue("@description", income.Description);
+                    command.Parameters.AddWithValue("@createAt", income.CreatedAt);
+                    command.Parameters.AddWithValue("@userid", income.UserId);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+
+
+
+        }
     }
 }
