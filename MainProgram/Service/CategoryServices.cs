a2836fa7cbf720e4b3c84a0f37c4c74c61b9c0d7
Joy Ivan <joyivan6@gmail.com>
2023-11-11 20:00:03 +0700

    add update, delete, and getlist services for category

--- MainProgram/Service/CategoryServices.cs
+++ MainProgram/Service/CategoryServices.cs
@@ -28,6 +28,69 @@ namespace MainProgram.Service
                 }
             }
         }
+
+        public static bool UpdateCategory(Category category)
+        {
+            DBConnection dbConnection = new DBConnection();
+            string query = "UPDATE Categories SET title = @title, type = @type WHERE categoryID = @id";
+            using (NpgsqlConnection  conn = dbConnection.GetConnection()) 
+            { 
+                conn.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@title", category.Title);
+                    command.Parameters.AddWithValue("@type", category.Type.ToString());
+                    command.Parameters.AddWithValue("@id", category.CategoryID);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+        public static bool DeleteCategory(int categoryId)
+        {
+            DBConnection dbConnection = new DBConnection();
+            string query = "DELETE FROM Categories WHERE categoryID = @id";
+            using (NpgsqlConnection conn = dbConnection.GetConnection())
+            {
+                conn.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@id", categoryId);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+        public static List<Category> GetCategories()
+        {
+            DBConnection dbConnection = new DBConnection();
+            List<Category> categories = new List<Category>();
+            string query = "SELECT * FROM Categories";
+            using (NpgsqlConnection connection = dbConnection.GetConnection()) 
+            { 
+                connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection)) 
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Category category = new Category(
+                                title: reader["Title"].ToString(),
+                                type: (Category.TransactionType)Enum.Parse(typeof(Category.TransactionType), reader["Type"].ToString())
+                            );
+                            categories.Add(category);
+                        }
+                    }
+                }
+            }
+
+            return categories;
+        }
     }
 }
 
