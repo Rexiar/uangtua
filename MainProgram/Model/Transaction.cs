f6009e1c871939916f136a4a8734d6dbbfa4d32c
Lia <dhiyaa.amalia.putri@mail.ugm.ac.id>
2023-11-16 11:54:04 +0700

    test

--- MainProgram/Model/Transaction.cs
+++ MainProgram/Model/Transaction.cs
@@ -8,9 +8,7 @@ using System.Transactions;
 namespace MainProgram.Model
 {
     public class Transaction
-    {
-        public int TransactionID { get; set; }
-        public int CategoryID { get; set; }
+    {   public int CategoryID { get; set; }
         public Category Category { get; set; }
         public int Amount { get; set; }
         public string Note { get; set; }
@@ -18,13 +16,13 @@ namespace MainProgram.Model
         public int UserID { get; set; }
         public User User { get; set; }
 
-        public Transaction(int transactionID, int categoryID, int amount, string note, int userID)
+        public Transaction(int transactionID, int categoryID, int amount, string note, int userID, DateTime date)
         {
-            TransactionID = transactionID;
             CategoryID = categoryID;
             Amount = amount;
             Note = note;
             UserID = userID;
+            Date = date;
         }
     }
 }
